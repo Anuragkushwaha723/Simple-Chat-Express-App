0812b990f5f473e916c9513735b914cbdcf1de95
AnuragKushwaha <anuragkushwaha723@gmail.com>
2023-03-16 15:23:33 +0530

    Routing from login page to user page

--- routes/login.js
+++ routes/login.js
@@ -2,11 +2,16 @@ const express=require('express');
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.send('<form action="/login/message" method="POST"><input type="text" name="username" placeholder="username"><br><button type="Submit">Login</button></form>')
+    res.send(`
+    <h2>Login page</h2>
+    <form onsubmit="localStorage.setItem('username',document.getElementById('username').value)" action="/login/product" method="POST">
+    <input id="username" type="text" name="title" placeholder="username">
+    <br>
+    <button type="submit">Login</button>
+    </form>`);
 });
-router.post('/message',(req,res,next)=>{
-    const object=req.body;
-    localStorage.setItem("username",object.username);
+
+router.post('/product',(req,res,next)=>{
     res.redirect('/');
 })
 module.exports=router;
--- routes/screen.js
+++ routes/screen.js
@@ -1,7 +1,27 @@
 const express=require('express');
+const fs=require('fs');
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.send('<h1>welcome</h1>');
-})
+    fs.readFile('message.txt',{utf:8},(err,data)=>{
+        if(err){
+            console.log(err);
+            data='No chat exist';
+        }
+        res.send(`
+        ${data}
+        <h2>Message App</h2>
+        <form onsubmit="document.getElementById('username').value=localStorage.getItem('username')" action="/" method="POST">
+        <input type="text" name="message" placeholder="message">
+        <input type="hidden" id="username" name="username">
+        <br>
+        <button type="submit">Send</button>
+        </form>`);
+    })
+});
+router.post('/',(req,res,next)=>{
+    fs.writeFile('./message.txt',`${req.body.username}:${req.body.message}`,{flag:'a'},(err)=>{
+        err ? console.log(err) : res.redirect('/');
+    });
+});
 module.exports=router;
